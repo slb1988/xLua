2f03197b86292138cc8ba29bd05db11cff7c810d
laibing.sun <laibing.sun@mihoyo.com>
2021-12-06 15:07:56 +0800

    update encode example

--- Assets/XLua/Examples/01_Helloworld/Helloworld.cs
+++ Assets/XLua/Examples/01_Helloworld/Helloworld.cs
@@ -6,6 +6,8 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 */
 
+using System;
+using System.IO;
 using UnityEngine;
 using XLua;
 
@@ -13,18 +15,44 @@ namespace XLuaTest
 {
     public class Helloworld : MonoBehaviour
     {
+        public TextAsset ta;
+
+        public byte[] RSACheckContent(byte[] data)
+        {
+            byte[] filecontent = new byte[data.Length - 128];
+            Array.Copy(data, 128, filecontent, 0, filecontent.Length);
+            return filecontent;
+        }
         // Use this for initialization
         void Start()
         {
             LuaEnv luaenv = new LuaEnv();
-            luaenv.DoString("CS.UnityEngine.Debug.Log('hello world')");
+
+            Debug.Log("lua start");
+            var luadatas = RSACheckContent(ta.bytes);
+            byte[] luascript_magickeys = new byte[] { 47, 79, 101, 157 };
+            for (int i = 0; i < luadatas.Length; i++)
+            {
+                luadatas[i] ^= luascript_magickeys[i % 4];
+            }
+
+            Debug.Log("lua dostring");
+            luaenv.DoString(luadatas, "battle_lua");
+            
+            Debug.Log("lua end");
+            //luaenv.DoString("CS.UnityEngine.Debug.Log('hello world')");
             luaenv.Dispose();
+            luaRight = true;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
 
+        bool luaRight = false;
+        private void OnGUI()
+        {
+            if (luaRight)
+            {
+                GUILayout.Button("hahaha");
+            }
         }
     }
 }
